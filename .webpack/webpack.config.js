b2603e9837fc0171960ee7a90e06c74e3c848841
cpagejs <3205214720@163.com>
2025-02-04 14:21:52 +0800

    webpack

--- .webpack/webpack.config.js
+++ .webpack/webpack.config.js
@@ -1,20 +1,18 @@
-const merge = require("webpack-merge");
-const baseWebpackConfig = require("./webpack.base.conf");
-
-// const path = require("path");
+const { merge } = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base");
 const webpack = require("webpack");
 
 module.exports = merge(baseWebpackConfig, {
   // 模式
   mode: "development",
   // 调试工具
-  devtool: "inline-source-map",
+  devtool: "source-map",
   // 开发服务器
   devServer: {
-    contentBase: false, // 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
+    // contentBase: false, // 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
     historyApiFallback: true, // 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
     compress: true, // 启用gzip压缩
-    inline: true, // 设置为true，当源文件改变时会自动刷新页面
+    // inline: true, // 设置为true，当源文件改变时会自动刷新页面
     hot: true, // 模块热更新，取决于HotModuleReplacementPlugin
     host: "127.0.0.1", // 设置默认监听域名，如果省略，默认为“localhost”
     port: 8703, // 设置默认监听端口，如果省略，默认为“8080”
@@ -23,7 +21,7 @@ module.exports = merge(baseWebpackConfig, {
   // 插件
   plugins: [
     // 热更新插件
-    new webpack.NamedModulesPlugin(),
+    // new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
   optimization: {
--- example/DemoConnect.js
+++ example/DemoConnect.js
@@ -1,52 +1,62 @@
-import React from 'react';
-import { connect } from '../src/index';
+import React from "react";
+import { connect } from "../src/index";
 
-function DemoConnect(props){
+function DemoConnect(props) {
   // console.log(props)
-  
-  const handelClick = ()=>{
-    // console.log(props.loading)
-    props.getUser({
-      id: 456,
-      name: '同步修改'
-    })
-    .then(res=>{
-      console.log(res)
-    })
-    .catch(err=>{
-      console.log(err)
-    })
-  }
 
-  const handelClickAsync = ()=>{
-    props.setNameAsync({
-      id: 789,
-      name: '异步修改'
-    })
-    .then(res=>{
-      console.log('handelClickAsync',res)
-    })
-  }
+  const handelClick = () => {
+    // console.log(props.loading)
+    props
+      .getUser({
+        id: 456,
+        name: "同步修改",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handelClickAsync = () => {
+    props
+      .setNameAsync({
+        id: 789,
+        name: "异步修改",
+      })
+      .then((res) => {
+        console.log("handelClickAsync", res);
+      });
+  };
 
   return (
     <div>
       <h1>connect---</h1>
-      <p>loading-setNameAsync->{props?.loading?.user?.setNameAsync ? <span>true</span> : <span>false</span>}</p>
+      <p>
+        loading-setNameAsync-
+        {props?.loading?.user?.setNameAsync ? (
+          <span>true</span>
+        ) : (
+          <span>false</span>
+        )}
+      </p>
       <p>id: {props.id}</p>
       <p>name: {props.name}</p>
       <button onClick={handelClick}>修改</button>
       <button onClick={handelClickAsync}>异步修改</button>
     </div>
-  )
+  );
 }
 
-const mapState = ({user:{id, name}, loading}) => ({
+const mapState = ({ user: { id, name }, loading }) => ({
   id,
   name,
-  loading
+  loading,
 });
-const mapDispatch = ({user:{getUser, getAuth, setNameAsync}}) => ({
+const mapDispatch = ({ user: { getUser, getAuth, setNameAsync } }) => ({
   getUser,
-  getAuth, setNameAsync
-})
-export default connect(mapState, mapDispatch)(DemoConnect)
+  getAuth,
+  setNameAsync,
+});
+export default connect(mapState, mapDispatch)(DemoConnect);
